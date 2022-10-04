b23ddb698c99c0fbc6bd8a239000dd6415a725c3
ffmustbe <77720735+ffmustbe@users.noreply.github.com>
2022-10-04 16:32:31 +0100

    ok

--- Untitled.R
+++ Untitled.R
@@ -1,15 +1,2 @@
-#practice
-library(tidyverse)
-ggplot(data = mpg)+
-  geom_point(mapping = aes(x = displ, y = hwy))
-ggplot(data = mpg) +
-  geom_point(mapping = aes(x = displ,y = hwy, color = class))
-ggplot(data = mpg)+
-  geom_point(mapping = aes(x = displ, y = hwy, size = class))
-ggplot(data = mpg)+
-  geom_point(mapping = aes(x = displ, y = hwy, alpha = class))
-ggplot(data = mpg)+
-  geom_point(mapping = aes(x = displ, y = hwy, shape = class))
-ggplot(data = mpg)+
-  geom_point(mapping = aes(x = displ, y = hwy), color = "blue")
-View(mpg)
+com<- random()
+
